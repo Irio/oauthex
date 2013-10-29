b519c36e732d17051f3f4e84c1a526edffc00d73
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-10-29 00:59:24 -0200

    Improve naming conventions and code style
    Changes in variable names was based in conventions of
    http://oauth.net/core/1.0/

--- lib/oauthex.ex
+++ lib/oauthex.ex
@@ -8,44 +8,43 @@ defmodule Oauthex do
   use Application.Behaviour
   require Lager
 
-  defrecord Consumer, key: nil, secret: nil, hash: :hmac_sha1
-  defrecord ReqInfo, token: nil, secret: nil
-  defrecord AccInfo, token: nil, secret: nil
+  defrecord Consumer,    key:   nil, secret: nil, hash: :hmac_sha1
+  defrecord RequestInfo, token: nil, secret: nil
+  defrecord AccountInfo, token: nil, secret: nil
 
   def start(_, _) do
   end
 
-  def access_token(url, params, consumer, reqinfo) do
-    result = get url, params, consumer, reqinfo
-    {token, secret} = token_info result
-    AccInfo.new token: token, secret: secret
+  def request_token(url, params, consumer) do
+    { token, secret } = get(url, params, consumer) |> token_info
+    RequestInfo.new token: token, secret: secret
   end
 
-  def request_token(url, params, consumer) do
-    result = get url, params, consumer
-    {token, secret} = token_info result
-    ReqInfo.new token: token, secret: secret
+  def access_token(url, params, consumer, request_info) do
+    { token, secret } = get(url, params, consumer, request_info) |> token_info
+    AccountInfo.new token: token, secret: secret
   end
 
   def token_info(result) do
     params = params_decode result
-    {token(params), token_secret(params)}
+    { token(params), token_secret(params) }
   end
 
   def consumer_to_tuple(consumer) do
-    {consumer.key, consumer.secret, consumer.hash}
+    { consumer.key, consumer.secret, consumer.hash }
   end
 
   def post(url, params, consumer) do
     assert_get :oauth.post url, params, consumer_to_tuple(consumer)
   end
 
-  def post(url, params, consumer, reqinfo) do
-    assert_get :oauth.post(
-      url, params, consumer_to_tuple(consumer),
-      reqinfo.token, reqinfo.secret, 
-      [{:sync, :false}, {:stream, :self}]
-    )
+  def post(url, params, consumer, request_info) do
+    assert_get :oauth.post(url,
+                           params,
+                           consumer_to_tuple(consumer),
+                           request_info.token,
+                           request_info.secret,
+                           [{ :sync, :false }, { :stream, :self }])
   end
 
   def get(url, consumer) do
@@ -53,24 +52,25 @@ defmodule Oauthex do
   end
 
   def get(url, params, consumer) do
-    assert_get :oauth.get(url, params, consumer_to_tuple (consumer))
+    assert_get :oauth.get(url, params, consumer_to_tuple(consumer))
   end
 
-  def get(url, params, consumer, reqinfo) do
+  def get(url, params, consumer, request_info) do
     assert_get :oauth.get(
-      url, params, consumer_to_tuple(consumer), reqinfo.token, reqinfo.secret
+      url, params, consumer_to_tuple(consumer), request_info.token, request_info.secret
     )
   end
 
   defp assert_get(result) do
     case result do
-      {:ok, {{_,200,_},_,_}=data} ->
+      { :ok, { { _, 200, _ }, _, _ } = data } ->
         Lager.debug 'Result: ~p', [data];
         data
-      {:ok, ref} -> ref
+      { :ok, ref } ->
+        ref
       result ->
         Lager.error 'oauth error ~p', [result]
-        raise {:oauth_error, result}
+        raise { :oauth_error, result }
     end
   end
 end
